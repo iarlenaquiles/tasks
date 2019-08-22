296357f1a5358a8f01c3dc0f191d4a2ad73d806b
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-08-21 22:24:51 -0300

    criando projeto tasks

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+const projects = [];
+
+server.get("/projects", (req, res) => {
+  return res.json(projects);
+});
+
+server.post("/projects", (req, res) => {
+  const { id, title } = req.body;
+  projects.push({ id, title, tasks: [] });
+  return res.json(projects);
+});
+
+server.post("/projects/:id/tasks", (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.tasks.push(title);
+
+  return res.json(project);
+});
+
+server.put("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.title = title;
+  return res.json(project);
+});
+
+server.delete("/projects/:id", (req, res) => {
+  const { id } = req.params;
+
+  const project = projects.find(p => p.id == id);
+  projects.splice(project, 1);
+  return res.send();
+});
+
+server.listen(3000);
